31137e21092209b198e77d81de6576c3f7d38126
Maria Tan <mkvht@outlook.com>
2022-05-05 19:13:43 +0200

    react app for heroku

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(
-    <h1>Hello World</h1>,
-    document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Movie Database</h1>
+      <ul>
+        <li>
+          <Link to={"/movies"}>List movies</Link>
+        </li>
+        <li>
+          <Link to={"/movies/new"}>Register new movie</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function ListMovies() {
+  return null;
+}
+
+function RegisterMovie() {
+  return null;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/movies"} element={<ListMovies />} />
+        <Route path={"/movies/new"} element={<RegisterMovie />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
